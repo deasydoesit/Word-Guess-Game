ba3e6de315abd151d593bfdf4d4a36b9baed8708
deasydoesit <darwin.rinderer@gmail.com>
2018-05-16 17:22:20 -0400

    Including comments to better describe proccessing flow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@
 
 //Need to configure reset functions when win occurs or loss occurs.
 
+//Globl variables used throughout
 var userWins = 0;
 var userLosses = 0;
 var gameCounter = 1;
@@ -13,11 +14,15 @@ var placeholder = [];
 
 var wordsBank = ["Han", "Luke", "Leial"];
 
+//Function to assign a word from wordsBank to the wordInstance. Creates random number
+//then pulls the array value from the index corresponding to the random number.
 function selectWord() {
     var randomNum = Math.floor(Math.random() * 3);
     return wordsBank[randomNum];
 }
 
+//Function to display blanks corresponding to the length of the wordInstance. Again, 
+//wordInstance holds the randomly selected word from the wordBanks array.
 function displayBlanks() {
     var blanks = document.getElementById("word");
     for (var i = 0; i < wordInstance.length; i++) {
@@ -26,6 +31,10 @@ function displayBlanks() {
     blanks.innerHTML = placeholder.join(" ");
 }
 
+//Function to identify whether a user guessed letter is in the wordInstance. In order
+//to account for words with repeat letters (e.g., banana), I'm creating an arrayOfHits 
+//which will hold the index corresponding to each matched letter corresponding to the 
+//user guessed letter. 
 function isGuessInWord(guess) {
     var arrayOfHits = [];
     var lowerCaseWordInstance = wordInstance.toLowerCase();
@@ -37,20 +46,22 @@ function isGuessInWord(guess) {
     return arrayOfHits;
 }
 
+//Function to display correctly guessed letter, if any. The guessed letter is passed as
+//an argument into letterGuessed, and the return value from isGuessInWord is passed 
+//into array. 
 function displayGuess(letterGuessed, array) {
-    var blanks = document.getElementById("word");
-    var wordToBeGuessed = document.getElementById("word");
+    var correctLetter = document.getElementById("word");
     for (var i = 0; i < array.length; i++) {
-        console.log(placeholder[array[i]]);
+        console.log("placeholder[array[i]] = " + placeholder[array[i]]);
         placeholder[array[i]] = letterGuessed;
+        console.log("placeholder = " + placeholder);
     }
-    blanks.innerHTML = placeholder.join(" ");
+    correctLetter.innerHTML = placeholder.join(" ");
 }
 
+//Function to identify if the user guessed the word. If so, update wins and game counter.
 function checkIfWordGuessed () {
-    console.log(placeholder.length);
     var wins = document.getElementById("wins");
-    var losses = document.getElementById("losses");
     var counter = document.getElementById("counter");
     if (placeholder.join("") === wordInstance.toLowerCase() && placeholder.length === wordInstance.length) {
         userWins++;
@@ -61,6 +72,7 @@ function checkIfWordGuessed () {
     }
 }
 
+//Function to identify if the user is out of guesses.
 function checkIfLost () {
     if (numOfGuesses === 0) {
         var losses = document.getElementById("losses");
@@ -69,43 +81,50 @@ function checkIfLost () {
     } 
 }
 
+//Function to update the user's guessed letters.
 function updateGuesses(letter) {
     userGuesses = userGuesses + letter + " " ;
     var guess = document.getElementById("userGuess");
     guess.innerHTML = userGuesses;
 }
 
-function myFunction() {
+//Function to start the game, which links up to the html input element.
+function startGame() {
 
+    //wordInstance for the game is selected
     wordInstance = selectWord();
+
+    //Blanks are displayed corresponded to the length of the wordInstance
     displayBlanks();
+
     console.log(wordInstance);
 
+    //Listening for user selection
     document.addEventListener('keyup', function (event) {
 
-        //storing user selected letter in variable keyName
+        //Storing user selected letter in variable keyName
         var keyName = event.key;
 
-        //generating array hit which indictes whether or not keyName is in wordInstance
+        //Generating array hit which indictes whether or not keyName is in wordInstance
         var hit = isGuessInWord(keyName);
 
-        //updating number of guesses left
+        //Updating number of guesses left
         if (hit.length === 0) {
             numOfGuesses--;
             var num = document.getElementById("num");
             num.innerHTML = "Number of Guesses Left: " + numOfGuesses;
         } 
 
-        //updating display of user guesses
+        //Updating display of user guesses
         updateGuesses(keyName, );
 
-        //updating guess hit
+        //Updating guess hit
         displayGuess(keyName, hit);
         
-        //user wins?
+        //User wins?
         checkIfWordGuessed();
 
-        //user loses?
+        //User loses?
         checkIfLost();
     });
     
